b5919feb32dac9fda36c057ca37e4cb30c3208eb
ipotemkin <91094458+ipotemkin@users.noreply.github.com>
2021-11-19 15:59:59 +0300

    renamed directors into objs in BasicDAO

--- dao/basic.py
+++ dao/basic.py
@@ -2,7 +2,6 @@
 # здесь в методах можно построить сложные запросы к БД
 
 # Например
-# from dao.model.directors import Director, DirectorBM
 from errors import NotFoundError, NoContentError, BadRequestError, DatabaseError
 
 
@@ -14,46 +13,46 @@ class BasicDAO:
         self.nested_schema = nested_schema
 
     def get_all(self):
-        if not (directors := self.session.query(self.model).all()):
+        if not (objs := self.session.query(self.model).all()):
             raise NotFoundError
-        return [self.schema.from_orm(director).dict() for director in directors]
+        return [self.schema.from_orm(obj).dict() for obj in objs]
 
     def get_one(self, did: int):
-        # if not (director := self.session.query(Director).get(did)):
+        # if not (obj := self.session.query(obj).get(did)):
         #     raise NotFoundError
-        director = self.session.query(self.model).get_or_404(did)
-        return self.schema.from_orm(director).dict()
+        obj = self.session.query(self.model).get_or_404(did)
+        return self.schema.from_orm(obj).dict()
 
-    def create(self, new_director: dict):
-        if not new_director:
+    def create(self, new_obj: dict):
+        if not new_obj:
             raise NoContentError
         try:
-            director = self.model(**new_director)
+            obj = self.model(**new_obj)
         except Exception:
             raise BadRequestError
         try:
-            self.session.add(director)
+            self.session.add(obj)
             self.session.commit()
         except Exception:
             raise DatabaseError
 
-    def update(self, new_director: dict, did: int):
-        if not new_director:
+    def update(self, new_obj: dict, did: int):
+        if not new_obj:
             raise NoContentError
         if not self.model.query.get(did):
             raise NotFoundError
-        if ('id' in new_director) and (did != new_director['id']):
+        if ('id' in new_obj) and (did != new_obj['id']):
             raise BadRequestError
         try:
-            self.session.query(self.model).filter(self.model.id == did).update(new_director)
+            self.session.query(self.model).filter(self.model.id == did).update(new_obj)
             self.session.commit()
         except Exception:
             raise DatabaseError
 
     def delete(self, did: int):
-        director = self.session.query(self.model).get_or_404(did)
+        obj = self.session.query(self.model).get_or_404(did)
         try:
-            self.session.delete(director)
+            self.session.delete(obj)
             self.session.commit()
         except Exception:
             raise DatabaseError
